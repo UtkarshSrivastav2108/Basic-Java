dc31c8bfd456858cd5f69fc21c514b52f9fe52fa
utkarsh srivastav <usrivastav11@gmail.com>
2021-07-26 13:03:15 +0530

    sdgasi

---
+++ 2. Operator and Expressions/Xor.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Xor {
+
+    public static void main(String[] args) {
+
+        int a = 9, b = 12, c;
+        c = a ^ b;
+        System.out.println(c);
+    }
+}
