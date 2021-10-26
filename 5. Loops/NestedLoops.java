29735b89710a0a1693d8d9be78673c7075f96991
utkarsh srivastav <usrivastav11@gmail.com>
2021-10-27 00:06:58 +0530

    loops

---
+++ 5. Loops/NestedLoops.java
@@ -0,0 +1,16 @@
+package com.practice.main;
+
+import java.util.Scanner;
+
+public class NestedLoops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a value");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.println(i + "," + j);
+            }
+        }
+    }
+}
