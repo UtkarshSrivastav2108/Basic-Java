e8244f885ee6b45bd69bc97ab6ed0063c3c28f60
Utkarsh Srivastav <usrivastav11@gmail.com>
2021-12-21 02:12:43 +0530

    just a check

---
+++ invertedHalfpyramid180.java
@@ -0,0 +1,14 @@
+public class invertedHalfpyramid180 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i >= 1; i--) {
+
+            for (var each : collection) {
+
+            }
+
+        }
+
+    }
+}
